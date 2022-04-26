db54a6fab2059d6be1bd5c36a6e69f54ed8dc45b
Birger Koblitz <git@birger-koblitz.de>
2022-04-26 21:01:10 +0200

    realtek: Cleanup of RTL930x switch ISR
    This cleans up the ISR of RTL930x switches. It also adds limits
    to link failure IRQs which are useful for debug purposes
    but can flood the system.
    
    Signed-off-by: Birger Koblitz <git@birger-koblitz.de>
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/rtl930x.c
+++ target/linux/realtek/files-5.10/drivers/net/dsa/rtl83xx/rtl930x.c
@@ -689,18 +689,25 @@ irqreturn_t rtl930x_switch_irq(int irq, void *dev_id)
 {
 	struct dsa_switch *ds = dev_id;
 	u32 ports = sw_r32(RTL930X_ISR_PORT_LINK_STS_CHG);
+	u32 media = sw_r32(RTL930X_MAC_LINK_MEDIA_STS);
+	static unsigned int err_cntr = 3;
 	u32 link;
 	int i;
 
 	/* Clear status */
 	sw_w32(ports, RTL930X_ISR_PORT_LINK_STS_CHG);
 
+	/*
+	 * It is not uncommon having to read the link status register twice.
+	 * At least the 93xx needs this with an external RTL8226 PHY, but
+	 * likely all connections.
+	 */
+	link = sw_r32(RTL930X_MAC_LINK_STS);
+	link = sw_r32(RTL930X_MAC_LINK_STS);
+	pr_debug("%s link status: %08x, media %08x\n", __func__, link, media);
+
 	for (i = 0; i < RTL930X_CPU_PORT; i++) {
 		if (ports & BIT(i)) {
-			/* Read the register twice because of issues with latency at least
-			 * with the external RTL8226 PHY on the XGS1210 */
-			link = sw_r32(RTL930X_MAC_LINK_STS);
-			link = sw_r32(RTL930X_MAC_LINK_STS);
 			if (link & BIT(i))
 				dsa_port_phylink_mac_change(ds, i, true);
 			else
@@ -713,6 +720,10 @@ irqreturn_t rtl930x_switch_irq(int irq, void *dev_id)
 	if (ports) {
 		pr_info("%s link faults: %08x\n", __func__, ports);
 		sw_w32(ports, RTL930X_ISR_SERDES_LINK_FAULT_P);
+		if (err_cntr == 0)
+			sw_w32(0, RTL930X_IMR_SERDES_LINK_FAULT_P);
+		else
+			err_cntr--;
 	}
 
 	/* Handle SDS RX symbol errors */
