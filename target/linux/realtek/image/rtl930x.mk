2aa2637336492c08ecc888037ef3d17cbe6acb15
Olliver Schinagl <oliver@schinagl.nl>
2022-07-26 08:57:26 +0200

    realtek: rtl930x: Add shared base for xgs12xx series
    The XGS1250-21, XGS1210-12 and XGS1010-12 are very similar, with very
    minor differences. Lets have a shared devicetree to make maintaining
    multiple devices with a common base easier.
    
    The new shared devicetree also drops the compatible partition layout
    with the OEM firmware, as it does not serve any purpose and only wastes
    space.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- target/linux/realtek/image/rtl930x.mk
+++ target/linux/realtek/image/rtl930x.mk
@@ -1,5 +1,38 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+define Device/kernel
+  KERNEL := \
+    kernel-bin | \
+    append-dtb | \
+    lzma | \
+    uImage lzma
+endef
+
+define Device/initramfs
+  KERNEL_INITRAMFS := \
+    kernel-bin | \
+    append-dtb | \
+    lzma | \
+    uImage lzma
+endef
+
+define Device/zyxel_initramfs
+  KERNEL_INITRAMFS := \
+    kernel-bin | \
+    append-dtb | \
+    lzma | \
+    zyxel-vers $$$$(ZYXEL_VERS) | \
+    uImage lzma
+endef
+
+define Device/zyxel_xgs1200
+  $(Device/kernel)
+  $(Device/initramfs)
+  DEVICE_VENDOR := Zyxel
+  IMAGE_SIZE := 15424k
+  SOC := rtl9302b
+endef
+
 define Device/zyxel_xgs1250-12
   SOC := rtl9302
   UIMAGE_MAGIC := 0x93001250
