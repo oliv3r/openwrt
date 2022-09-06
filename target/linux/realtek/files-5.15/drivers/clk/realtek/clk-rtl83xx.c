757d5d39f4fd8bbae1519273fd05032c7fe04fc5
Markus Stockhausen <markus.stockhausen@gmx.de>
2022-09-06 10:48:45 +0200

    realtek: clk: Do not return fixed clock rate
    If ctrl0 and/or ctrl1 return 0, we have other issues, and if all 0 is
    valid, then we shouldn't hack around it. Lets remove this hardcoded
    default.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

--- target/linux/realtek/files-5.15/drivers/clk/realtek/clk-rtl83xx.c
+++ target/linux/realtek/files-5.15/drivers/clk/realtek/clk-rtl83xx.c
@@ -381,9 +381,6 @@ static unsigned long rtcl_recalc_rate(struct clk_hw *hw, unsigned long parent_ra
 
 	switch (rtcl_ccu->soc) {
 	case SOC_RTL838X:
-		if ((ctrl0 == 0) && (ctrl1 == 0) && (clk->idx == CLK_LXB))
-			return 200000000;
-
 		cmu_divn2_selb = FIELD_GET(RTL838X_PLL_CTRL1_CMU_DIVN2_SELB, ctrl1);
 		cmu_divn3_sel = rtcl_divn3[FIELD_GET(RTL838X_PLL_CTRL1_CMU_DIVN3_SEL, ctrl1)];
 		break;
